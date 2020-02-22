09c21863c0bd0cb83d92672d655e04674ad8984a
Dunse <dgrege17@student.aau.dk>
2020-02-22 18:07:45 +0100

    Lavet på start skærmen

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,8 +1,12 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_sizeguide/backend/costumer_info.dart';
+import 'package:flutter_sizeguide/widgets/size_input_field.dart';
 
 class StartScreen extends StatelessWidget {
   final String _title;
+  TextEditingController heightTextController = new TextEditingController();
+  CustomerInfo _customerInfo;
 
   StartScreen(this._title);
 
@@ -10,11 +14,16 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
+          centerTitle: true,
           title: Text(_title),
         ),
-        body: Center(),
+        body: Center(
+          child: new Column(
+            children: <Widget>[
+              SizeInputField('Height', heightTextController)
+            ],
+          ),
+        ),
     );
   }
 }
